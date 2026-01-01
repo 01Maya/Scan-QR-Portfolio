3342cfb46ed4859f24ceb37b4b263c49ac8f5705
Mansi_Patel <patelmaya9812@gmail.com>
2026-01-02 00:11:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Mansi Portfolio QR Code',
+  title: 'Mansi Portfolio QR Code✨',
   description: 'My Work QR Code',
 }
 
