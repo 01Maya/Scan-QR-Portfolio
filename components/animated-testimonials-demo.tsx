c0cbb18a808a193a64670d303eff5f07bc60965b
Mansi_Patel <patelmaya9812@gmail.com>
2026-01-03 00:17:34 +0530

    Update animated-testimonials-demo.tsx

--- components/animated-testimonials-demo.tsx
+++ components/animated-testimonials-demo.tsx
@@ -54,8 +54,8 @@ export default function AnimatedTestimonialsDemo() {
     },
     {
       quote:
-        "An elegant and responsive landing page with smooth animations, created to showcase stunning interior spaces with style and sophistication.",
-      name: <a href="https://luxor-interior-design.vercel.app/" target="_blank">Luxor - Interior Design</a>,
+        "An elegant and responsive Interior landing page with smooth animations, created to showcase stunning interior spaces with style and sophistication.",
+      name: <a href="https://luxor-interior-design.vercel.app/" target="_blank">Luxor Design</a>,
       designation: "Interior Design Landing Page 🏡",
       src: "interior.png",
     },
